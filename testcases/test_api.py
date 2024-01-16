9d4b6919e2d64d86189572cafe9e43c0d87c4a59
ZhaoJixiang <zhaojixiang@zhnrh.com>
2024-01-16 15:09:56 +0800

    first commit

---
+++ testcases/test_api.py
@@ -0,0 +1,82 @@
+import requests
+import jsonpath
+import re
+
+
+class TestApi:
+    # 类变量
+    access_token = ''
+    csrf_token = ''
+    sess = requests.Session()
+
+    # 获取access_token接口
+    def test_get_token(self):
+        urls = 'https://api.weixin.qq.com/cgi-bin/token'
+        datas = {'grant_type': 'client_credential',
+                 'appid': 'wx8a9de038e93f77ab',
+                 'secret': '8326fc915928dee3165720c910effb86'}
+        # res = requests.get(url=urls, params=datas)
+        res = TestApi.sess.request('get', url=urls, params=datas)
+        # lis = jsonpath.jsonpath(res.json(), '$.access_token')
+        TestApi.access_token = res.json()['access_token']
+        print(TestApi.access_token)
+
+    def test_select_flag(self):
+        urls = 'https://api.weixin.qq.com/cgi-bin/tags/get'
+        datas = {'access_token': TestApi.access_token}
+        # res = requests.get(url=urls, params=datas)
+        res = TestApi.sess.request('get', url=urls, params=datas)
+        print(res.json())
+
+    # 编辑标签接口
+    def test_edit_flag(self):
+        urls = 'https://api.weixin.qq.com/cgi-bin/tags/update'
+        params = {'access_token': TestApi.access_token}
+        datas = {"tag": {"id": 134, 'name': '广东人'}}
+        # res = requests.post(url=urls, json=datas, params=params)
+        res = TestApi.sess.request('post', url=urls, json=datas, params=params)
+
+        print(res.json())
+
+    def test_file_upload(self):
+        urls = 'https://api.weixin.qq.com/cgi-bin/media/uploadimg'
+        params = {'access_token': TestApi.access_token}
+        datas = {'media': open(r"D:\aaa.jpg", 'rb')}
+        # res = requests.post(url=urls, files=datas, params=params)
+        res = TestApi.sess.request('post', url=urls, files=datas, params=params)
+
+        print(res.json())
+
+    def test_phpwind(self):
+        urls = 'http://47.107.116.139/phpwind/'
+        # res = requests.get(url=urls)
+        res = TestApi.sess.request('get', url=urls)
+        # print(res.text)
+        TestApi.csrf_token = re.search('name="csrf_token" value="(.*?)"', res.text).group(1)
+        print(TestApi.csrf_token)
+
+    def test_login(self):
+        urls = 'http://47.107.116.139/phpwind/index.php?m=u&c=login&a=dorun'
+        headers = {
+            'Accept': 'application/json, text/javascript, /; q=0.01',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+        datas = {
+            'username': 'baili',
+            'password': 'baili123',
+            'csrf_token': TestApi.csrf_token,
+            'backurl': 'http://47.107.116.139/phpwind/',
+            'invite': ''
+        }
+        # res = requests.post(url=urls, data=datas, headers=headers)
+        res = TestApi.sess.request('post', url=urls, data= datas, headers=headers)
+        print(res.json())
+
+
+if __name__ == '__main__':
+    TestApi().test_get_token()
+    TestApi().test_select_flag()
+    TestApi().test_edit_flag()
+    TestApi().test_file_upload()
+    TestApi().test_phpwind()
+    TestApi().test_login()
